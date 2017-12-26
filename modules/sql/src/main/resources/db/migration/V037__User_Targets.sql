7a0ffe52b3dbb6a52693047874b486226f13c39d
Shane Walker <swalker@gemini.edu>
2017-12-26 10:22:47 -0300

    Renamed user target migration and switched to TEXT

--- modules/sql/src/main/resources/db/migration/V037__User_Targets.sql
+++ modules/sql/src/main/resources/db/migration/V037__User_Targets.sql
@@ -9,10 +9,10 @@
 -- observation apart from the main science targets.
 
 CREATE TABLE e_user_target_type (
-  id         identifier            PRIMARY KEY,
-  short_name character varying(20) NOT NULL,
-  long_name  character varying(20) NOT NULL,
-  obsolete   boolean               NOT NULL
+  id         identifier PRIMARY KEY,
+  short_name TEXT       NOT NULL,
+  long_name  TEXT       NOT NULL,
+  obsolete   boolean    NOT NULL
 );
 
 ALTER TABLE e_user_target_type OWNER TO postgres;
@@ -29,10 +29,10 @@ Other	Other	Other	f
 -- Associates a target, user target type, and an observation.
 
 CREATE TABLE user_target (
-  id               SERIAL                PRIMARY KEY,
-  target_id        integer               NOT NULL REFERENCES target(id),
-  user_target_type identifier            NOT NULL REFERENCES e_user_target_type(id),
-  observation_id   character varying(40) NOT NULL REFERENCES observation ON DELETE CASCADE
+  id               SERIAL     PRIMARY KEY,
+  target_id        integer    NOT NULL REFERENCES target(id),
+  user_target_type identifier NOT NULL REFERENCES e_user_target_type(id),
+  observation_id   TEXT       NOT NULL REFERENCES observation ON DELETE CASCADE
 );
 
 ALTER TABLE user_target OWNER TO postgres;
