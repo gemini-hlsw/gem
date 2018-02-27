638f900b854d5f6620d467b1e1fb3f44c549a91a
Shane Walker <swalker@gemini.edu>
2018-02-27 12:40:16 -0300

    Adds an empty fetch command

--- modules/telnetd/src/main/scala/command/fetch.scala
+++ modules/telnetd/src/main/scala/command/fetch.scala
@@ -6,11 +6,16 @@ package telnetd
 package command
 
 import cats.implicits._
+import com.monovore.decline.{ Command => _, _ }
+import tuco._
+import Tuco._
+import tuco.shell._
 
-/**
-  *
+/** Command to fetch an observation or program from an OCS2 ODB.
   */
 object fetch {
+
+  type EitherId = Either[Program.Id, Observation.Id]
 
   val host: Opts[String] =
     Opts.option[String](
@@ -20,14 +25,23 @@ object fetch {
       long    = "host"
     ).withDefault("localhost")
 
-  val id: Opts[Either[Program.Id, Observation.Id]]
+  val id: Opts[EitherId] =
     Opts.argument[String](
       metavar = "id"
     ).mapValidated { s =>
-      (Observation.Id.fromString(s).map(Either.right) orElse ProgramId.fromString(s).map(Either.left))
-        .fold(Validated.invalidNel(s"Could not parse '$s' as an observation or program id")) { e =>
-          Validated.valid(e)
-        }
+      val id = Observation.Id.fromString(s).map(_.asRight[Program.Id]) orElse
+                 ProgramId.fromString(s).map(_.asLeft[Observation.Id])
+
+      id.toValidNel(s"Could not parse '$s' as an observation or program id")
     }
 
+  val command: GemCommand =
+    Command(
+      "fetch", "Fetch an observation or program from an OCS2 ODB.",
+      (host, id).mapN { (h: String, id: EitherId) => (d: GemState) => {
+        for {
+          _ <- writeLn(s"fetch $h $id")
+        } yield d
+      }}
+    ).zoom(Session.data[GemState])
 }
